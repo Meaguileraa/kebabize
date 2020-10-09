0ab08c8f7cd1a5a51bd908ea076649344d6c9143
Marissa Aguilera <marissaguilera@yahoo.com>
2020-10-09 15:57:12 -0700

    Test cases

--- solution.py
+++ solution.py
@@ -6,3 +6,9 @@
 # kebabize('camelsHaveThreeHumps') // camels-have-three-humps
 # kebabize('camelsHave3Humps') // camels-have-h
 
+def kebabize(string):
+    """Given a string in camel case convert it to kebab case."""
+
+
+print(kebabize('camelsHaveThreeHumps')) #camels-have-three-humps
+print(kebabize('camelsHave3Humps')) #camels-have-h
